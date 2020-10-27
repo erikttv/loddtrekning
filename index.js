d86d01f07131addd9c1a2a0f549b15b0b905f45b
erikttv <erthinn73@gmail.com>
2020-10-27 22:31:02 +0100

    full deal

--- index.js
+++ index.js
@@ -3,16 +3,19 @@ let liste = []
 function addname(){
     let name = document.getElementById('navn').value;
     let antall = document.getElementById('antall').value;
+    if(name == null || antall == null || name == '' || antall == ''){
+        return;
+    }
     for (let index = 0; index < antall; index++) {
         liste.push([name, antall]);
     }
     let text = document.getElementById('personer').innerHTML;
     text += `<p onclick="removename(this.id)" id="`+name+`">Navn: `+name+` Antall: `+antall+`</p>`;
     document.getElementById('personer').innerHTML = text;
+    document.getElementById('antall').value = '';
+    document.getElementById('navn').value = '';
 }
 
-
-//Usikker om det funker hvis mellomrom på same fornavn!
 
 function removename(name){
     var element = document. getElementById(name);
@@ -27,5 +30,12 @@ function removename(name){
 
 
 function startlotteri(){
-    
+    if(liste.length == 0 ){
+        document.getElementById('vinnertekst').innerHTML = "Vinner: Ingen ble registert!"
+    } else {
+        let random = Math.floor(Math.random() * liste.length)
+        winner = liste[random];
+        name = winner[0];
+        document.getElementById('vinnertekst').innerHTML = "Vinner: " + name;
+    }
 }
